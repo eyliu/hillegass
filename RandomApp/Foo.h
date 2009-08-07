13a626d41f67768fbd4bdf0ce529aec8bcdaaefc
Elson Liu <eyliu.dev@umich.edu>
2009-08-07 16:26:23 -0400

    Added Foo class

---
+++ RandomApp/Foo.h
@@ -0,0 +1,16 @@
+//
+//  Foo.h
+//  RandomApp
+//
+//  Created by Elson Liu on 8/7/09.
+//  Copyright 2009 University of Michigan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Foo : NSObject {
+
+}
+
+@end
---
+++ RandomApp/Foo.m
@@ -0,0 +1,14 @@
+//
+//  Foo.m
+//  RandomApp
+//
+//  Created by Elson Liu on 8/7/09.
+//  Copyright 2009 University of Michigan. All rights reserved.
+//
+
+#import "Foo.h"
+
+
+@implementation Foo
+
+@end
