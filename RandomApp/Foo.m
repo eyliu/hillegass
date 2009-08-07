470530f3f8a470a22383958f71074fc9cd441e35
Elson Liu <eyliu.dev@umich.edu>
2009-08-07 16:41:16 -0400

    Implemented Foo

--- RandomApp/Foo.m
+++ RandomApp/Foo.m
@@ -11,4 +11,23 @@
 
 @implementation Foo
 
+- (IBAction)generate:(id)sender
+{
+	// Generate a number between 1 and 100 inclusive
+	int generated;
+	generated = (random() % 100) + 1;
+	
+	NSLog(@"generated = %d", generated);
+	
+	// Ask the text field to change what it is displaying
+	[textField setIntValue:generated];
+}
+
+- (IBAction)seed:(id)sender
+{
+	// Seed the random number generator with the time
+	srandom(time(NULL));
+	[textField setStringValue:@"Generator seeded"];
+}
+
 @end
