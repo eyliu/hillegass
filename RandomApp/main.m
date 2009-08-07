83022b05cf63e7610d56e87b696f517bcd249b30
Elson Liu <eyliu.dev@umich.edu>
2009-08-07 15:59:43 -0400

    Created RandomApp

---
+++ RandomApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RandomApp
+//
+//  Created by Elson Liu on 8/7/09.
+//  Copyright University of Michigan 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
